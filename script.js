d1879ed06fa89ab25097f008ec59677cfd57ae0a
ruansx <112948189+ruanssxx@users.noreply.github.com>
2024-05-23 08:13:52 -0300

    777777777

---
+++ script.js
@@ -0,0 +1,11 @@
+const caixaPrincipal = document.querySelector(".caixa-principal")
+const caixaPerguntas = document.querySelector(".caixa-perguntas")
+const caixaAlternativas = document.querySelector(".caixa-alternativas")
+const caixaResultado = document.querySelector(".caixa-resultado")
+const TextoResultado = document.querySelector(".texto-resultado")
+
+const perguntas = [
+    enunciado: "Pergunta1"
+    alternativas: [ "Alternativa 1", "Alternativa 2 "]
+    
+    , "Pergunta2 "]
